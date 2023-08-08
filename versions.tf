e715459cb18159c0c595da0d967d7c8ea67c7d58
desilva <desilva@ubiwhere.com>
2023-08-08 22:10:00 +0100

    fix: Remove provider configuration from module

--- versions.tf
+++ versions.tf
@@ -11,17 +11,3 @@ terraform {
   }
 }
 
-# Configure proxmox provider
-provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true
-}
-provider "aws" {
-  s3_use_path_style           = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-}
